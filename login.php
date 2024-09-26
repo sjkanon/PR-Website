f19b5020dc25470f9eac111bf1a9a0111866448d
Sjoerd Kanon <sjoerd@kanon.email>
2024-09-26 11:27:29 +0000

    Sep 26, 2024, 1:27 PM

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
 <?php
 session_start();
-$conn = new mysqli('localhost', 'pr_djm', '' , 'techniekdjm_')
+$conn = new mysqli('localhost', 'pr_djm', '_7vd3nC37' , 'techniekdjm_');
+
+if ($conn->connect_error){
+    die("Verbindingsfout: " . $conn->connect_error);
+}
+
