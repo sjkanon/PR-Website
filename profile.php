ed6ba0b30ce450299cd30da6e7148361d33249c0
Sjoerd Kanon <Sjoerd@kanon.email>
2024-09-27 13:51:22 +0000

    Sep 27, 2024, 3:51 PM

--- profile.php
+++ profile.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['gebruikersnaam'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profiel</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/admin.css">
 </head>
 <body>
 <nav class="navbar">
