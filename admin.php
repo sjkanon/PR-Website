316f430e720f712be725a7fd8e481aeabbef13fd
Sjoerd Kanon <Sjoerd@kanon.email>
2024-09-27 13:42:55 +0000

    Sep 27, 2024, 3:42 PM

--- admin.php
+++ admin.php
@@ -44,7 +44,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/admin.css">
 </head>
 <body>
     <!-- Navigatiebalk -->
